a4ce0809b4adac8b5575888327885b0e492cf585
hht28 <hht28@drexel.edu>
2025-01-16 08:40:12 -0500

    create, get, delete author test

--- starter/src/server.test.ts
+++ starter/src/server.test.ts
@@ -5,34 +5,14 @@ let host = "localhost";
 let protocol = "http";
 let baseUrl = `${protocol}://${host}:${port}`;
 
-test("GET /foo?bar returns message", async () => {
-    let bar = "xyzzy";
-    let { data } = await axios.get(`${baseUrl}/foo?bar=${bar}`);
-    expect(data).toEqual({ message: `You sent: ${bar} in the query` });
-});
+test("Create, fetch, and delete an author", async () => {
+    let author = { id: "1", name: "Author Name", bio: "Author Bio" };
 
-test("GET /foo returns error", async () => {
-    try {
-        await axios.get(`${baseUrl}/foo`);
-    } catch (error) {
-        // casting needed b/c typescript gives errors "unknown" type
-        let errorObj = error as AxiosError;
-        // if server never responds, error.response will be undefined
-        // throw the error so typescript can perform type narrowing
-        if (errorObj.response === undefined) {
-            throw errorObj;
-        }
-        // now, after the if-statement, typescript knows
-        // that errorObj can't be undefined
-        let { response } = errorObj;
-        // TODO this test will fail, replace 300 with 400
-        expect(response.status).toEqual(300);
-        expect(response.data).toEqual({ error: "bar is required" });
-    }
-});
+    await axios.post(`${baseUrl}/authors`, author);
 
-test("POST /bar works good", async () => {
-    let bar = "xyzzy";
-    let result = await axios.post(`${baseUrl}/foo`, { bar });
-    expect(result.data).toEqual({ message: `You sent: ${bar} in the body` });
-});
+    let { data } = await axios.get(`${baseUrl}/authors/1`);
+    expect(data).toMatchObject(author);
+
+    let deleteResponse = await axios.delete(`${baseUrl}/authors/1`);
+    expect(deleteResponse.data).toEqual({ message: "Author deleted successfully" });
+  });
